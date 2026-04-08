ace0c514c8b2bea232c0a877a9e162c1ee4e2be8
Nishkal2010 <nishkal2010@users.noreply.github.com>
2026-04-07 19:08:53 -0500

    feat: add hash router to render ComparePage at #compare

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { ComparePage } from "./components/ComparePage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+function Root() {
+  const [hash, setHash] = useState(window.location.hash);
+  useEffect(() => {
+    const onHash = () => setHash(window.location.hash);
+    window.addEventListener("hashchange", onHash);
+    return () => window.removeEventListener("hashchange", onHash);
+  }, []);
+  return hash === "#compare" ? <ComparePage /> : <App />;
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
-)
+);
